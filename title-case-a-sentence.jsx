d97fc10b8c1cc152aec7402ccd59996b5a0eebe6
Jamari Goodridge <47260720+JamariCJGoodridge@users.noreply.github.com>
2019-03-03 19:47:03 -0500

    Update and rename title-case-a-sentence.json to title-case-a-sentence.jsx

---
+++ title-case-a-sentence.jsx
@@ -0,0 +1,14 @@
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  return splitStr.join(' ');
+  }
+}
+
+// CAPITALIZES FIRST LETTER OF EVERY WORD
+
+// EXAMPLE
+
+console.log(titleCase("I'm a little tea pot")); // I'm A Little Tea Pot
