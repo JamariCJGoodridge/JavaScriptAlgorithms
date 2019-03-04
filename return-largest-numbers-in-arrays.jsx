c03b0859f9c4927f008dd58174aaf1796dd1fc38
Jamari Goodridge <47260720+JamariCJGoodridge@users.noreply.github.com>
2019-03-03 19:34:40 -0500

    Update return-largest-numbers-in-arrays.jsx

--- return-largest-numbers-in-arrays.jsx
+++ return-largest-numbers-in-arrays.jsx
@@ -16,6 +16,10 @@ function largestOfFour(arr) {
   return newArr;
 }
 
+// LOOPS THROUGH ARRAYS WITH SUBARRAYS TO FIND THE LARGEST NUMBER IN EACH SUBARRAY
+
+// EXAMPLE
+
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])); // [5,27,39,1001]
 console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]])); // [27,5,39,1001]
 console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]])); // [9,35,97,1000000]
